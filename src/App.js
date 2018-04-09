c8af195ccc8539fc3e16f48ea0fa3154aef31e86
ali1129js <ali1129js@gmail.com>
2018-04-09 10:57:35 +0200

    mapping es6 over hardcoded list one component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-09T10:46:38+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-09T10:55:31+02:00
+ */
+
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const people = [
+  {
+    id:1,
+    first:"Ali",
+    last:"me",
+    age:33
+  },
+  {
+    id:2,
+    first:"Sam",
+    last:"Dha",
+    age:44
+  },
+  {
+    id:3,
+    first:"Toni",
+    last:"Jane",
+    age:22
+  }
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          people.map(person =>
+            <div key={person.id}>
+              <h1> {person.name} </h1>
+              <h1> {person.last} </h1>
+              <h1> {person.age} </h1>
+            </div>
+            )
+        }
       </div>
     );
   }
