90519b7ba11fa0c576b1764b9935ce3f6fa21bc3
ali1129js <ali1129js@gmail.com>
2018-04-12 18:37:36 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-09T10:46:38+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-09T12:00:40+02:00
+ * @Last modified time: 2018-04-09T12:26:45+02:00
  */
 
 
@@ -61,8 +61,8 @@ class App extends Component {
         </form>
         {people.filter(theFilter(searchInput)).map( person =>
           <div key={person.id}>
-            <h3>{person.first}</h3>
-            <h3>{person.last}</h3>
+            <h3>{person.first}{person.last}</h3>
+            <h3>{person.age}</h3>
           </div>
         )}
       </div>
